9bbe650dff686327817ab546b301b1f136808978
Yuki Okushi <zx.you.funy@gmail.com>
2018-10-21 22:34:14 +0900

    Get args

--- tsuncon.go
+++ tsuncon.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
 func main() {
 	fmt.Println("ξ ﾟ⊿ ﾟ)ξ < Hello, World!")
+	flag.Parse()
+	args := flag.Args()
+	fmt.Print("ξ ﾟ⊿ ﾟ)ξ < args is ")
+	fmt.Println(args)
 }
