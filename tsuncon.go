54d6a653469a210d5cf81399ef9ee2ca413ba989
Yuki Okushi <zx.you.funy@gmail.com>
2018-10-21 22:45:19 +0900

    Print usage

--- tsuncon.go
+++ tsuncon.go
@@ -6,9 +6,20 @@ import (
 )
 
 func main() {
-	fmt.Println("ξ ﾟ⊿ ﾟ)ξ < Hello, World!")
 	flag.Parse()
 	args := flag.Args()
-	fmt.Print("ξ ﾟ⊿ ﾟ)ξ < args is ")
-	fmt.Println(args)
+	if len(args) == 0 {
+		printUsage()
+	} else {
+		fmt.Print("ξ ﾟ⊿ ﾟ)ξ < args is ")
+		fmt.Println(args)
+		fmt.Println(args[0])
+	}
+}
+
+func printUsage() {
+	fmt.Println("ξ ﾟ⊿ ﾟ)ξ < Hello, I'm Tsunko!")
+	fmt.Println("ξ ﾟ⊿ ﾟ)ξ < I will tell you how many your contributions on GitHub.")
+	fmt.Println("ξ ﾟ⊿ ﾟ)ξ < Please input and run 'tsuncon <your GitHub name>'")
+	fmt.Println("ξ ﾟ⊿ ﾟ)ξ < Bye!")
 }
